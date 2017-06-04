9fef3ae837ae155f8b3815ddabab28b8c28e59ac
Jesse <jesse.ranon@gmail.com>
2017-06-03 20:17:06 -0700

    Edited for loops to print upper limit to console.

--- script.js
+++ script.js
@@ -32,31 +32,31 @@ console.log(829%13);
 
 //9. Create a for loop that counts from 0 to 130 by 3s
 
-for (var i=0; i<130; i+=3) {
+for (var i=0; i<=130; i+=3) {
 	console.log(i);
 }
 
 //10. Create a for loop that counts from 3 to 17 by 2s
 
-for (var i=3;i<17;i+=2) {
+for (var i=3;i<=17;i+=2) {
 	console.log(i);
 }
 
 //11. Create a for loop that counts from 100 to 3 by -1
 
-for (var i=100;i>3;i--) {
+for (var i=100;i>=3;i--) {
 	console.log(i);
 }
 
 //12. Create a for loop that counts from 1 to 100 by 1s
 
-for (var i=1;i<100;i++) {
+for (var i=1;i<=100;i++) {
 	console.log(i);
 }
 
 //13. Create a for loop that counts from 1 to 100, but instead of printing `i` prints `fizz` if the number is divisible by 5
 
-for (var i=1;i<100;i++) {
+for (var i=1;i<=100;i++) {
 	if (i%5==0) {
 		console.log('fizz');
 	} else {
@@ -66,7 +66,7 @@ for (var i=1;i<100;i++) {
 
 //14. Create a for loop that counts from 1 to 100, but instead of printing `i` prints `buzz` if the number is divisible by 3
 
-for (var i=1;i<100;i++) {
+for (var i=1;i<=100;i++) {
 	if (i%3==0) {
 		console.log('buzz');
 	} else {
@@ -76,7 +76,7 @@ for (var i=1;i<100;i++) {
 
 //15. Create a for loop that counts from 1 to 100, but instead of printing `i` prints `fizzbuzz` if the number is divisible by 15
 
-for (var i=1;i<100;i++) {
+for (var i=1;i<=100;i++) {
 	if (i%15==0) {
 		console.log('fizzbuzz');
 	} else {
@@ -96,7 +96,7 @@ Hint: Use your last three loops and if/then/else statements. You can learn about
 
 */
 
-for (var i=1;i<100;i++) {
+for (var i=1;i<=100;i++) {
 	if (i%15==0) {
 		console.log('fizzbuzz');
 	} else if (i%5==0) {
